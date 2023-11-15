85ca95c31e4086de319e2c83456f7c3a3f80f6b7
iwmaster <118463764+iwmaster@users.noreply.github.com>
2023-11-15 17:26:30 -0500

    Update caption-img.js

--- blocks/caption-img/caption-img.js
+++ blocks/caption-img/caption-img.js
@@ -1,4 +1,5 @@
 export default function decorate(block) {
     /* grab the caption text and use as alt */
-    
+    const imgElement = document.getElementsByTagName("img");
+    imgElement.setAttribute("alt","Alt Text");
  }
