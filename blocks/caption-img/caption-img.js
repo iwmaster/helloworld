415dee8eab2ac06ee8562ba90d6cbd94d5cf344c
iwmaster <118463764+iwmaster@users.noreply.github.com>
2023-11-15 17:12:49 -0500

    Update caption-img.js

--- blocks/caption-img/caption-img.js
+++ blocks/caption-img/caption-img.js
@@ -1,2 +1,7 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
+
+export default function decorate(block) {
+    /* grab the caption text and use as alt */
+    
+ }
